b9c70f0e3ffc75f8e23ba0d745bfd9553d0763df
Dennserrano <dennserrano@gmail.com>
2021-10-26 13:30:53 +0200

    innerhtmllev1_3

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+let gallery = document.getElementById('gallery')
+
+gallery.innerHTML = '<figure><img src="https://picsum.photos/200" alt=""><figcaption>Bild1</figcaption></figure><figure><img src="https://picsum.photos/200" alt=""><figcaption>Bild2</figcaption></figure><figure><img src="https://picsum.photos/200" alt=""><figcaption>Bild3</figcaption></figure>'
